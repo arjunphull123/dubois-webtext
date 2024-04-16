b2ea19a7fe697517ced7ce8d245e677542e862da
arjunphull123 <arjunphull@arizona.edu>
2024-04-16 15:15:30 -0700

    Added tabs and other updates

--- public/main.js
+++ public/main.js
@@ -11,3 +11,40 @@ document.addEventListener('DOMContentLoaded', (event) => {
     };
     window.addEventListener('scroll', toggleVisibility);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Select all chevron SVG elements
+    const chevrons = document.querySelectorAll('.chevron');
+
+    // Function to toggle visibility and rotate chevron
+    function toggleSection(event) {
+        const chevron = event.currentTarget;
+        const subsectionBody = chevron.parentElement.nextElementSibling;
+        const subsection = subsectionBody.parentElement
+
+        // Toggle a class for rotating the chevron
+        chevron.style.transform = chevron.style.transform === 'rotate(180deg)' ? 'rotate(0deg)' : 'rotate(180deg)';
+
+        // Toggle the 'expanded' class for the adjacent section body
+        subsectionBody.classList.toggle('expanded');
+        subsection.classList.toggle('expanded')
+    }
+
+    // Add click event listeners to each chevron
+    chevrons.forEach(chevron => {
+        chevron.addEventListener('click', toggleSection);
+    });
+
+    const tabs = document.querySelectorAll('.tab')
+
+    function activateTab() {
+      tabs.forEach(tab => {
+        tab.classList.remove('active')
+        this.classList.add('active')
+      })
+    }
+
+    tabs.forEach(tab => {
+      tab.addEventListener('click', activateTab)
+    })
+});
